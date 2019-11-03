3e20fe127b9b97e5fb42ebe1d99a129adaf38365
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-03 19:36:25 +0100

    Update loggin.php

--- loggin.php
+++ loggin.php
@@ -8,8 +8,8 @@
 
 <div id="beholder">
   <form action="checked.php" method="post">
-    <input type="text" placeholder="User" required/>
-    <input type="password" placeholder="Password" required/>
+    <input type="text" name="brukernavn" placeholder="User" required/>
+    <input type="password" name="passord" placeholder="Password" required/>
     <input type="submit" class="button" />
  </form>
 </div>
