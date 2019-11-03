3621606eb7b17342dee0044e22e6fda8ad264456
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-03 03:43:48 +0100

    Create check.php
    Starten av prosjektet . første del.

---
+++ check.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+</title>
+</head>
+<body>
+
+<?php
+  $name = $_POST[""];
+  $pass = $_POST[""];
+  
+  //hardcode if (name === name etc) or sql checker...
+  
+  function checkdb(){
+    
+  }
+  
+?>
+
+
+</body>
+</html>
