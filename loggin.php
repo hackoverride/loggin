12e8efadf4e5457e3d5c0c701f71dfef38f999b7
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-03 03:33:52 +0100

    Create loggin.php

---
+++ loggin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+
+</head>
+<body>
+
+<div id="beholder">
+  <form action="checked.php" method="post">
+    <input type="text" placeholder="User" required/>
+    <input type="password" placeholder="Password" required/>
+    <input type="submit" class="button" />
+ </form>
+</div>
+
+</body>
+</html>
