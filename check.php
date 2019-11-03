f0034a1e3a0fff64f9dc8f39d4ee34abb9136c30
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-03 19:34:09 +0100

    Update check.php
    La til MySql checker for passord.

--- check.php
+++ check.php
@@ -7,14 +7,53 @@
 <body>
 
 <?php
-  $name = $_POST[""];
-  $pass = $_POST[""];
+  $access = false;
+  $name = $_POST["name"];
+  $pass = $_POST["password"];
   
-  //hardcode if (name === name etc) or sql checker...
+  /*HARDCODE: if ($name == "Admin" && $pass == "Password1") {
+  echo '';
+  } else {
+  echo 'Wrong password';
+  }*/
   
-  function checkdb(){
-    
+  //Code to check a MySql server:
+  
+  $servername = "localhost";
+  $username = "username";
+  $password = "password";
+  $dbname = "myDB";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
   }
+
+  $sql = "SELECT id, brukernavn, passord FROM tabell";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          echo "id: " . $row["id"]. " - Name: " . $row["brukernavn"]. " " . $row["passord"]. "<br>";
+          if ($row["brukernavn] == $name && $row["passord"] == $pass) {
+          echo 'Welcome ' .$name;
+          $access = true;
+          }
+      }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+  
+  if ($access){
+  //send user to correct area
+  } else {
+  echo 'Feil passord eller brukernavn! ';
+  }
+
   
 ?>
 
