31dcaaf5661cc89727ee53879d92824a747d3b74
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-04 04:41:34 +0100

    Update check.php

--- check.php
+++ check.php
@@ -1,62 +1,82 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>
-</title>
-</head>
-<body>
+    <head>
+        <title>
+            
+        </title>
+        <style>
+            body{background-color: #B8D2D5; width: 100%; height: 100%; margin: 20px; font-size: 300%;}
+        </style>
+    </head>
+    <body>
+        <?php
 
-<?php
-  $access = false;
-  $name = $_POST["brukernavn"];
-  $pass = $_POST["passord"];
-  
-  /*HARDCODE: if ($name == "Admin" && $pass == "Password1") {
-  echo '';
-  } else {
-  echo 'Wrong password';
-  }*/
-  
-  //Code to check a MySql server:
-  
-  $servername = "localhost";
-  $username = "username";
-  $password = "password";
-  $dbname = "myDB";
+        $ny = $_POST["new"];
+        $aktiv = false;
+        if ($ny == "on"){
+            
+            //Sjekk om brukernavn allerede finnes.
+            brukerSjekk("ny");
+            
+        } else {
+            //sjekker DB om bruker eksisterer og om passord stemmer...
+            brukerSjekk("sjekk");
+            
+            if ($aktiv){
+                echo 'Velkommen! Du har tastet riktig brukernavn og passord.';
+            //Kode her!
+            } else {
+                echo 'Feil bruker eller passord.';
+            }
+        }
+        
+        
+        function brukerSjekk($inn){
+            $servername = " x";
+            $username = " x";
+            $password = " x";
+            $dbname = " x";
+            
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            
+            if($inn == "sjekk"){
+            $sql = "SELECT Bruker, Passord FROM Brukere";
+            $result = $conn->query($sql);
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  }
-
-  $sql = "SELECT id, brukernavn, passord FROM tabell";
-  $result = $conn->query($sql);
-
-  if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-          echo "id: " . $row["id"]. " - Name: " . $row["brukernavn"]. " " . $row["passord"]. "<br>";
-          if ($row["brukernavn] == $name && $row["passord"] == $pass) {
-          echo 'Welcome ' .$name;
-          $access = true;
-          }
-      }
-  } else {
-      echo "0 results";
-  }
-  $conn->close();
-  
-  if ($access){
-  //send user to correct area
-  } else {
-  echo 'Feil passord eller brukernavn! ';
-  }
-
-  
-?>
-
-
-</body>
+            if ($result->num_rows > 0) {
+            // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    if ($row["Bruker"] == $_POST["bruker"] && $row["Passord"] == $_POST["passord"]){
+                        $aktiv = true;
+                    }
+                    
+                    
+                    
+                }
+            } else {
+            echo "Ingen brukere med det brukernavnet.";
+            }
+            
+            } else if ($inn == "ny") {
+                $kom = date("j F Y");
+                $sql = "INSERT INTO `Brukere`(`Bruker`, `Fornavn`, `Etternavn`, `Passord`, `Kommentar`) VALUES (".$_POST['bruker'].",".$_POST['bruker'].",'N/A',".$_POST['passord'].",".$kom.")";
+                $result = $conn->query($sql);
+                if ($conn->query($sql) === TRUE) {
+                    echo "New record created successfully";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            }
+            $conn->close();
+        }
+        
+        ?>
+        
+        
+    </body>
 </html>
