d88a7f4960eecae203663a1bdf80acf7bcc335a8
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-23 09:03:30 +0100

    Update loggin.php

--- loggin.php
+++ loggin.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="author" content="Michael Lund">
 <title>Login</title>
 
 </head>
@@ -8,6 +11,7 @@
 
 <div id="beholder">
   <form action="checked.php" method="post">
+    <!-- Think that with placeholder there is no need for label except for style -->
     <input type="text" name="brukernavn" placeholder="User" required/>
     <input type="password" name="passord" placeholder="Password" required/>
     <input type="submit" class="button" />
